5b4fa5f11916849cd42c82917e5b10a33b68c0f2
Fernando Gurgel <gurgelff@gmail.com>
2022-10-16 00:58:17 -0300

    impr: storybook accessibility | aria label

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -5,7 +5,7 @@ export interface CheckboxProps {}
 
 export const Checkbox = ({}: CheckboxProps) => {
   return (
-    <CheckboxPrimitive.Root className="w-6 h-6 p-[2px] bg-gray-800 rounded">
+    <CheckboxPrimitive.Root aria-label="Checkbox for remember me option" className="w-6 h-6 p-[2px] bg-gray-800 rounded">
       <CheckboxPrimitive.Indicator asChild>
         <Check weight="bold" className="h-5 w-5 text-cyan-500" />
       </CheckboxPrimitive.Indicator>
